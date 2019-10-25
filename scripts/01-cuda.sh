e1a0dd973f07349668f5eeff60cb7d07e4db1314
ptaylor <paul.e.taylor@me.com>
2019-10-25 11:02:36 -0700

    symlink include dir

--- scripts/01-cuda.sh
+++ scripts/01-cuda.sh
@@ -12,10 +12,22 @@ sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda
 sudo add-apt-repository -y "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
 sudo apt update && sudo apt -y install cuda
 
+# Install cuda-toolkit 9.2
+wget https://developer.nvidia.com/compute/cuda/9.2/Prod2/local_installers/cuda_9.2.148_396.37_linux
+sudo sh cuda_9.2.148_396.37_linux --toolkit --toolkitpath=/usr/local/cuda-9.2 --silent
+rm -rf cuda_9.2.148_396.37_linux
+
+if [ ! -d /usr/local/cuda-9.2/targets/x86_64-linux ]; then
+    sudo mkdir -p /usr/local/cuda-9.2/targets/x86_64-linux
+    sudo ln -s /usr/local/cuda-9.2/include /usr/local/cuda-9.2/targets/x86_64-linux/include
+fi
+
+# Install cuda-toolkit 10.0
 wget https://developer.nvidia.com/compute/cuda/10.0/Prod/local_installers/cuda_10.0.130_410.48_linux
 sudo sh cuda_10.0.130_410.48_linux --toolkit --toolkitpath=/usr/local/cuda-10.0 --silent
 rm -rf cuda_10.0.130_410.48_linux
 
-wget https://developer.nvidia.com/compute/cuda/9.2/Prod2/local_installers/cuda_9.2.148_396.37_linux
-sudo sh cuda_9.2.148_396.37_linux --toolkit --toolkitpath=/usr/local/cuda-9.2 --silent
-rm -rf cuda_9.2.148_396.37_linux
+if [ ! -d /usr/local/cuda-10.0/targets/x86_64-linux ]; then
+    sudo mkdir -p /usr/local/cuda-10.0/targets/x86_64-linux
+    sudo ln -s /usr/local/cuda-10.0/include /usr/local/cuda-10.0/targets/x86_64-linux/include
+fi
