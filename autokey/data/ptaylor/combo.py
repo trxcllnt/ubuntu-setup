32b04434426e0d48f13130e20efa5a6c7d6c876d
ptaylor <paul.e.taylor@me.com>
2020-04-28 18:04:04 -0700

    update autokey bindings for xfce4-terminal accels

--- autokey/data/ptaylor/combo.py
+++ autokey/data/ptaylor/combo.py
@@ -2,21 +2,20 @@
 
 import re
 
-#if re.match('^((?!.*Emacs).)*$', window.get_active_class()):
-if re.match('.*(xfce4-terminal)', window.get_active_class()) is not None:
-    h = store.get_global_value('hotkey')
-    if h == '<alt>+t':
-        keyboard.send_keys('<ctrl>+<shift>+t')
-    elif h == '<alt>+v':
-        keyboard.send_keys('<ctrl>+<shift>+v')
-    elif h == '<alt>+a':
-        keyboard.send_keys('<ctrl>+<shift>+a')
-    elif h == '<alt>+f':
-        keyboard.send_keys('<ctrl>+<shift>+f')
-    else:
-        keyboard.send_keys(h)
-else:
-    keyboard.send_keys(store.get_global_value('keycmd'))
+hotkey = store.get_global_value('hotkey')
+keycmd = store.get_global_value('keycmd')
 
 store.remove_global_value('hotkey')
 store.remove_global_value('keycmd')
+
+#if re.match('^((?!.*Emacs).)*$', window.get_active_class()):
+if re.match('.*(xfce4-terminal)', window.get_active_class()) is not None:
+    # If "Find" window is open, send translated key command.
+    # Otherwise send alt version so terminal accels are used.
+    keyboard.send_keys(
+        hotkey
+        if window.get_active_title() != 'Find' else 
+        keycmd
+    )
+else:
+    keyboard.send_keys(keycmd)
