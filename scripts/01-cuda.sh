44cc1d285c952d7f88721efca1bfde6585980f42
ptaylor <paul.e.taylor@me.com>
2020-09-30 19:45:19 -0700

    remove older CUDA toolkits, install g++/cc/c++ alternatives to follow gcc's version

--- scripts/01-cuda.sh
+++ scripts/01-cuda.sh
@@ -5,53 +5,28 @@ set -o errexit
 
 cd $(dirname "$(realpath "$0")")/../
 
-# Install gcc 7 and 8
-sudo apt install -y gcc-7 g++-7 gcc-8 g++-8
+# Install gcc 7, 8, 9, and 10
+sudo apt install -y gcc-7 g++-7 gcc-8 g++-8 gcc-9 g++-9 gcc-10 g++-10
+
+# Remove any existing gcc and g++ alternatives 
+sudo update-alternatives --remove-all cc  >/dev/null 2>&1 || true
+sudo update-alternatives --remove-all c++ >/dev/null 2>&1 || true
+sudo update-alternatives --remove-all gcc >/dev/null 2>&1 || true
+sudo update-alternatives --remove-all g++ >/dev/null 2>&1 || true
 
 # Use Ubuntu's update-alternatives utility to switch between GCC versions
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 0
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 0
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 0
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-8 0
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 100
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-9 100
+for x in 7 8 9 10; do
+    sudo update-alternatives \
+        --install /usr/bin/gcc gcc /usr/bin/gcc-$x ${x}0 \
+        --slave /usr/bin/cc cc /usr/bin/gcc-$x \
+        --slave /usr/bin/g++ g++ /usr/bin/g++-$x \
+        --slave /usr/bin/c++ c++ /usr/bin/g++-$x \
+        --slave /usr/bin/gcov gcov /usr/bin/gcov-$x
+done;
 
 # Make gcc-7 the default while we install the older CUDA toolkits
 sudo update-alternatives --set gcc /usr/bin/gcc-7
 sudo update-alternatives --set g++ /usr/bin/g++-7
-
-# Install cuda-toolkit 9.2
-wget -O /tmp/cuda_9.2.run \
-    https://developer.nvidia.com/compute/cuda/9.2/Prod2/local_installers/cuda_9.2.148_396.37_linux
-sudo sh /tmp/cuda_9.2.run --toolkit --toolkitpath=/usr/local/cuda-9.2 --silent
-rm -rf /tmp/cuda_9.2.run
-
-if [ ! -d /usr/local/cuda-9.2/targets/x86_64-linux ]; then
-    sudo mkdir -p /usr/local/cuda-9.2/targets/x86_64-linux
-    sudo ln -s /usr/local/cuda-9.2/include /usr/local/cuda-9.2/targets/x86_64-linux/include
-fi
-
-# Install cuda-toolkit 10.0
-wget -O /tmp/cuda_10.0.run \
-    https://developer.nvidia.com/compute/cuda/10.0/Prod/local_installers/cuda_10.0.130_410.48_linux
-sudo sh /tmp/cuda_10.0.run --toolkit --toolkitpath=/usr/local/cuda-10.0 --silent
-rm -rf /tmp/cuda_10.0.run
-
-if [ ! -d /usr/local/cuda-10.0/targets/x86_64-linux ]; then
-    sudo mkdir -p /usr/local/cuda-10.0/targets/x86_64-linux
-    sudo ln -s /usr/local/cuda-10.0/include /usr/local/cuda-10.0/targets/x86_64-linux/include
-fi
-
-# Install cuda-toolkit 10.1
-wget -O /tmp/cuda_10.1.run \
-    https://developer.download.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda_10.1.243_418.87.00_linux.run
-sudo sh /tmp/cuda_10.1.run --toolkit --toolkitpath=/usr/local/cuda-10.1 --silent
-rm -rf /tmp/cuda_10.1.run
-
-if [ ! -d /usr/local/cuda-10.1/targets/x86_64-linux ]; then
-    sudo mkdir -p /usr/local/cuda-10.1/targets/x86_64-linux
-    sudo ln -s /usr/local/cuda-10.1/include /usr/local/cuda-10.1/targets/x86_64-linux/include
-fi
 
 # Install cuda-toolkit 10.2
 wget -O /tmp/cuda_10.2.run \
@@ -64,11 +39,12 @@ if [ ! -d /usr/local/cuda-10.2/targets/x86_64-linux ]; then
     sudo ln -s /usr/local/cuda-10.2/include /usr/local/cuda-10.2/targets/x86_64-linux/include
 fi
 
+CURRENT_CUDA_VERSION="$(/usr/local/cuda/bin/nvcc --version | head -n4 | tail -n1 | cut -d' ' -f5 | cut -d',' -f1)"
+
 # Install update-alternatives for each CUDA version
-sudo update-alternatives --install /usr/local/cuda cuda /usr/local/cuda-9.2 0
-sudo update-alternatives --install /usr/local/cuda cuda /usr/local/cuda-10.0 0
-sudo update-alternatives --install /usr/local/cuda cuda /usr/local/cuda-10.1 0
-sudo update-alternatives --install /usr/local/cuda cuda /usr/local/cuda-10.2 100
+sudo update-alternatives --install /usr/local/cuda cuda /usr/local/cuda-10.2 0
+# set the latest apt-installed CUDA version as the highest priority
+sudo update-alternatives --install /usr/local/cuda cuda /usr/local/cuda-$CURRENT_CUDA_VERSION 100
 
 if [ ! "$(grep CUDA_HOME ~/.bashrc)" ]; then
     echo '
